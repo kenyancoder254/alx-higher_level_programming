aacfb509ca9a9c54afb7318bb25c16f4d8d9bfa1
kenyancoder254 <kenyancoder05@gmail.com>
2024-06-05 18:20:35 +0300

    Define a class square with private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""Initializes the size of a square"""
+
+
+class Square:
+    """
+    Initializes size of private variable
+    Args:
+        size: length of the square
+    """
+    def __init__(self, size):
+        self.__size = size
