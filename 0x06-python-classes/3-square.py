87da45d8df1e4fa70e147b54055540803b7fd0ae
kenyancoder254 <kenyancoder05@gmail.com>
2024-06-10 10:46:17 +0300

    Return the area of a sqaure

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+""" Defines class square based on 1-square.py"""
+
+
+class Square:
+    """Stores the value for square"""
+    def __init__(self, size=0):
+        """Initializes square and raises TypeError and ValueError respectively
+
+        Args:
+            param1 (int): Size of the square
+
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square based on size"""
+        return self.__size ** 2
