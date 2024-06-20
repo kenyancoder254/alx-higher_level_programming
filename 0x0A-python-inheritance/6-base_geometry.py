05409d7173f7d672f4187771fef2f2fdda7fc87c
kenyancoder254 <kenyancoder05@gmail.com>
2024-06-20 13:27:50 +0300

    Create a public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates Empty class"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+    def area(self):
+        """Issues a warning to the user"""
+        raise Exception("area() is not implemented")
