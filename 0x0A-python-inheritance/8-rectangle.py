61bb2701cb7b65a3e1c6413b58f54b965f789bc2
kenyancoder254 <kenyancoder05@gmail.com>
2024-06-20 16:46:29 +0300

    Initialize the values of a rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Creates Empty class"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        """Issues a warning to the user"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates input value"""
+        self.name = name
+        self.value = value
+        if isinstance(self.value, int) is not True:
+            raise TypeError(f"{self.name} must be an integer")
+        if self.value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """initializes the values of a rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
