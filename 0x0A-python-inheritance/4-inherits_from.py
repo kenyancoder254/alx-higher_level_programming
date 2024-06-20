9725b7b8eab75ac97d61b178a8bec82629e5529f
kenyancoder254 <kenyancoder05@gmail.com>
2024-06-20 13:14:55 +0300

    Check if an object is an instance of a class that inherited(directly or indirectly)

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Checks if an object is an instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """Checks for inheritance either directly or indirectly"""
+    obj_class = obj.__class__
+    if issubclass(obj_class, a_class) is True:
+        return True
+    else:
+        return False
