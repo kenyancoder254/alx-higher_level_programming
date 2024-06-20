790873001cdf81858f0b06eecbc32f7f9809aaf2
kenyancoder254 <kenyancoder05@gmail.com>
2024-06-20 17:11:57 +0300

    Implement area method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Creates Empty class"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self, width, height):
+        """Issues a warning"""
+        raise Exception("area() not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates input value"""
+        self.name = name
+        self.value = value
+        if isinstance(self.value, int) is not True:
+            raise TypeError(f"{self.name} must be an integer")
+        if self.value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """initializes the values of a rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
