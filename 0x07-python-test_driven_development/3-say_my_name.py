0ee23c361eb46cf77dcfce57d19aeb5a0300c16b
kenyancoder254 <kenyancoder05@gmail.com>
2024-07-01 17:09:05 +0300

    Print ther user's name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Prints the first and last name of user"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints last and first name of the user
+
+    Args:
+        first_name(str): user's first name
+        last_name(str): user's last name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}$")
